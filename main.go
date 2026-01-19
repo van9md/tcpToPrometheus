34005e5eeb9ccc5940d62b19f7eb253508fedaaf
van9md <van9md@gmail.com>
2026-01-19 13:30:11 +0300

    add prometheus metrics

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 	"net/http"
 	"sync"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type response struct {
@@ -17,18 +20,28 @@ type response struct {
 
 var addr string
 var result response
+var frequencyGauge = prometheus.NewGaugeVec(
+	prometheus.GaugeOpts{
+		Name: "accelerator_signal_frequency_hertz",
+		Help: "main frequency of linac",
+	}, []string{"tip"},
+)
 
 func api(w http.ResponseWriter, r *http.Request) {
 	b, err := json.Marshal(result)
 	if err != nil {
 		log.Println("Cannot marshall")
 	}
+	w.WriteHeader(http.StatusOK)
 	w.Write(b)
 	if err != nil {
 		log.Println("Cannot write to api")
 	}
 }
+
 func main() {
+	reg := prometheus.NewRegistry()
+	reg.MustRegister(frequencyGauge)
 	var wg sync.WaitGroup
 	flag.StringVar(&addr, "addr", "127.0.0.1:52550", "Address of tcp server, leave empty to simulate server")
 
@@ -38,6 +51,7 @@ func main() {
 		if err != nil {
 			log.Println("Cannot up test server")
 		}
+
 		go func(ln net.Listener) {
 			defer wg.Done()
 			wg.Add(1)
@@ -59,6 +73,8 @@ func main() {
 	}
 
 	time.Sleep(1 * time.Second)
+	log.Println("Server is on ", addr)
+
 	go func() {
 		defer wg.Done()
 		b := make([]byte, 4096)
@@ -83,13 +99,17 @@ func main() {
 					log.Println("unmarshal error")
 					break
 				}
-				log.Println(result.Frequency)
+				frequencyGauge.WithLabelValues("main frequency of linac").Set(float64(result.Frequency))
+				//log.Println(result.Frequency)
 				time.Sleep(1 * time.Second)
 			}
 			time.Sleep(5 * time.Second)
 		}
 	}()
 	http.HandleFunc("/api", api)
+	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
+	log.Println("api running on :8080/api")
+	log.Println("metrics running on :8080/metrics")
 	http.ListenAndServe(":8080", nil)
 	wg.Wait()
 	log.Println("ended")
